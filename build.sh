b65c1675595d387cf8372f3f7129277c0b2c5ebb
Nykyta Nykolaytsev <kupcenkonikita73@gmail.com>
2024-07-30 14:00:17 +0300

    fix build

--- build.sh
+++ build.sh
@@ -2,14 +2,15 @@
 # Exit on error
 set -o errexit
 
+# Update package list and install system dependencies
+apt-get update
+apt-get install -y somepackage
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
-
-apt-get update
-apt-get install -y somepackage
 
 # Apply any outstanding database migrations
 python manage.py migrate
